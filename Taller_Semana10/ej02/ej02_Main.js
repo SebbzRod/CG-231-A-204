b9050c343fd2dc13e26312e1121a2fdfde2b3059
SebbzRod <124273952+SebbzRod@users.noreply.github.com>
2023-04-02 21:05:21 -0500

    Update ej02_Main.js

--- Taller_Semana10/ej02/ej02_Main.js
+++ Taller_Semana10/ej02/ej02_Main.js
@@ -43,13 +43,15 @@ var lado = 10;
  * createCono: Construye un cono y los retorna
  * ENTRADAS: Base = Variable num, tamaño del lado de la base del cono que se creara en la escena. Ademas sera el valor de la altura
  *           Lados = Variable num, numero de lados del cono que se creara en la escena.
- * SALIDAS: cubo = Array con los objetos Mesh de Three.js correspondientes a cada cubo.
+ * SALIDAS: cone = El objeto cone, que representa el cono creado a partir de los parámetros proporcionados.
  */
 function crearCono( base, lado) {
+  
   const geometry = new THREE.ConeGeometry( base, base, lado);
   const material = new THREE.MeshNormalMaterial();
   const cone = new THREE.Mesh(geometry, material);
   return cone;
+  
 }
 
 const cono = crearCono(base, lado);
